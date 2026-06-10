2c4cfc496c706362ebf1368b6beaa34d4eb80d0e
RxSajib <sajibroy206@gmail.com>
2026-06-10 21:13:31 +0600

    feat: component design livetag

---
+++ app/src/main/java/com/zenbyte/studio/wavesphere/ui/component/NowPlayingComponent.kt
@@ -0,0 +1,14 @@
+package com.zenbyte.studio.wavesphere.ui.component
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun NowPlayingComponent() {
+
+}
+
+@Composable
+fun NowPlayingComponentPreview(modifier: Modifier = Modifier) {
+    NowPlayingComponent()
+}
