9f49165bb8e6ba7a23b893f0427acb38099c8019
RxSajib <sajibroy206@gmail.com>
2026-06-11 17:46:49 +0600

    feat: component design for premium tag

--- app/src/main/java/com/zenbyte/studio/wavesphere/MediaPlayerViewModel.kt
+++ app/src/main/java/com/zenbyte/studio/wavesphere/MediaPlayerViewModel.kt
@@ -5,9 +5,11 @@ import androidx.lifecycle.ViewModel
 import androidx.media3.common.MediaItem
 import com.zenbyte.studio.domain.model.MyChannel
 import com.zenbyte.studio.wavesphere.app.MyApplication
+import com.zenbyte.studio.wavesphere.utils.MyCustomLogger
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
+private const val TAG = "MediaPlayerViewModel"
 @HiltViewModel
 class MediaPlayerViewModel @Inject constructor() : ViewModel() {
 
@@ -21,7 +23,7 @@ class MediaPlayerViewModel @Inject constructor() : ViewModel() {
             }
 
         } catch (e: Exception) {
-
+            MyCustomLogger.logMessageInfo(tag = TAG, message = e.message?: "Wrong on media player play media")
         }
     }
 }
