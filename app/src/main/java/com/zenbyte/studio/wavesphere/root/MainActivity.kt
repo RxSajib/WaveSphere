5b709542471a00d95a5c8541f6f85bbd04f8a4b1
RxSajib <sajibroy206@gmail.com>
2026-06-20 12:06:43 +0600

    feat: (ui) ui design for Channel By Country

--- app/src/main/java/com/zenbyte/studio/wavesphere/root/MainActivity.kt
+++ app/src/main/java/com/zenbyte/studio/wavesphere/root/MainActivity.kt
@@ -4,8 +4,10 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.ui.Modifier
 import com.zenbyte.studio.wavesphere.ui.navigation.BottomAppBarNavigation
 import com.zenbyte.studio.wavesphere.ui.navigation.RootNavigation
@@ -19,7 +21,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             WaveSphereTheme {
-                    Box(modifier = Modifier.fillMaxSize()){
+                    Box(modifier = Modifier.fillMaxSize().background(color = MaterialTheme.colorScheme.surface)){
                         RootNavigation()
                     }
 
--- app/src/main/java/com/zenbyte/studio/wavesphere/ui/screen/channelByCountry/ChannelByCountryScreen.kt
+++ app/src/main/java/com/zenbyte/studio/wavesphere/ui/screen/channelByCountry/ChannelByCountryScreen.kt
@@ -1,21 +1,33 @@
 package com.zenbyte.studio.wavesphere.ui.screen.channelByCountry
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import coil3.compose.LocalPlatformContext
+import com.zenbyte.studio.domain.model.MyChannel
 import com.zenbyte.studio.presentation.viewmodel.getChannelByCountry.GetChannelByCountryViewModel
+import com.zenbyte.studio.wavesphere.MediaPlayerViewModel
 import com.zenbyte.studio.wavesphere.ui.component.ChannelItem
+import com.zenbyte.studio.wavesphere.ui.component.MyCustomAppBar
+import com.zenbyte.studio.wavesphere.ui.component.MyCustomStation
 
 @Composable
 fun ChannelByCountryScreen(modifier: Modifier = Modifier) {
@@ -23,29 +35,37 @@ fun ChannelByCountryScreen(modifier: Modifier = Modifier) {
     val contextCoil = LocalPlatformContext.current
     val viewModel : GetChannelByCountryViewModel = hiltViewModel()
     val channelListState by viewModel.channelList.collectAsStateWithLifecycle()
-
+    val mediaPlayerViewModel : MediaPlayerViewModel = hiltViewModel()
     val items = remember(channelListState) { channelListState }
 
-    LazyColumn(
-        modifier = modifier.fillMaxSize(),
-    ) {
-        itemsIndexed(
-            items = items,
-            key = { _, item -> item.stationuuid },
-            contentType = { _, _ -> "channel" }
-        ) { index, myChannel ->
-            ChannelItem(
-                myChannel = myChannel,
-                context = contextCoil,
-                modifier = Modifier
-            )
-            if (index < items.lastIndex) {
-                HorizontalDivider(
-                    modifier = Modifier.padding(horizontal = 16.dp),
-                    thickness = 0.5.dp,
-                    color = MaterialTheme.colorScheme.outlineVariant.copy(alpha = 0.5f)
-                )
+    Scaffold(
+        topBar = {
+            MyCustomAppBar(
+                title = "Bangladesh"
+            ) {
+
+            }
+        }
+    ) {innerPadding ->
+        Box(modifier = Modifier.fillMaxSize().padding(innerPadding)){
+            LazyVerticalGrid(
+                columns = GridCells.Fixed(3),
+                modifier = Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.spacedBy(10.dp),
+                horizontalArrangement = Arrangement.spacedBy(10.dp),
+                contentPadding = PaddingValues(16.dp)
+            ) {
+                items(items.size) { index ->
+                    MyCustomStation(
+                        context = contextCoil, myChannel = items[index],
+                        onClick = {myChannel ->
+                            mediaPlayerViewModel.playMusic(myChannel = myChannel)
+                        }
+                    )
+                }
             }
         }
     }
+
+
 }
